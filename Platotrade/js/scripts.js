c4fc5b60cd1ef71fa4e5fe7cc8a8dc4f7e542810
Roman Svistel <romansvistellviv@gmail.com>
2018-08-28 18:00:01 +0300

    Bootstrap & jQuery removed

--- Platotrade/js/scripts.js
+++ Platotrade/js/scripts.js
@@ -1,38 +1,10 @@
-// $(window).on('resize scroll', function() {
-//     if ($('#setup-steps-section').isInViewport()) {
-//         $('#menu').addClass('light-scheme').removeClass('mixed-scheme')
-//     } else if ($('#testimonial').isInViewport()) {
-//         $('#menu').addClass('fixed-scheme').removeClass('light-scheme')
-//     } else {
-//         $('#menu').removeClass('light-scheme').removeClass('fixed-scheme')
-//     }
-// });
 function openLogin() {
     document.getElementById('auth__in').style.display = 'block';
     document.getElementById('menu-links').style.display = 'none';
     document.getElementById('demo-image').style.display = 'none';
 }
-// document.getElementById("btn-login").onclick = function () {
-//     document.getElementById('auth__in').style.display = 'block';
-//     document.getElementById('menu-links').style.display = 'none';
-//     document.getElementById('demo-image').style.display = 'none';
-// };
-// $('#btn-login').click(function () {
-//     $('.auth__in').show();
-//     $('#menu-links').hide();
-//     $('.demo-image').hide()
-// });
-
-// indicators.click(reloadVideos(),checkSlideShow());
 checkSlideShow();
-
-// var indicatorsCollection = document.getElementsByClassName('carousel-indicators');
-// var indicators = indicatorsCollection[0].getElementsByTagName('li');
 function checkSlideShow() {
-    // if (indicators[0].classList.contains('active')) {
-    //     reloadVideos();
-    //     console.log('video reloaded')
-    // }
     setTimeout(function() {
         reloadVideos();
         console.log('video reloaded');
@@ -46,16 +18,6 @@ function reloadVideos() {
         item[i].load()
     }
 }
-
-// $.fn.isInViewport = function() {
-//     var elementTop = $(this).offset().top;
-//     var elementBottom = elementTop + $(this).outerHeight();
-//
-//     var viewportTop = $(window).scrollTop();
-//     var viewportBottom = viewportTop + $(window).height();
-//
-//     return elementBottom > viewportTop && elementTop < viewportBottom;
-// };
 
 function openMenu() {
     var x = document.getElementById("menu");
