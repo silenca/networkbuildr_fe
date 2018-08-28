c2b350a8b0685830ac1279531963ad025f6e36a3
Roman Svistel <romansvistellviv@gmail.com>
2018-08-28 11:45:32 +0300

    First animations added

--- Platotrade/js/scripts.js
+++ Platotrade/js/scripts.js
@@ -1,29 +1,61 @@
-$(document).ready(function () {
-    $(window).on('resize scroll', function() {
-        if ($('#setup-steps-section').isInViewport()) {
-            $('#menu').addClass('light-scheme').removeClass('mixed-scheme')
-        } else if ($('#testimonial').isInViewport()) {
-            $('#menu').addClass('fixed-scheme').removeClass('light-scheme')
-        } else {
-            $('#menu').removeClass('light-scheme').removeClass('fixed-scheme')
-        }
-    });
-    $('#btn-login').click(function () {
-        $('.auth__in').show();
-        $('#menu-links').hide();
-        $('.demo-image').hide()
-    });
-});
+// $(window).on('resize scroll', function() {
+//     if ($('#setup-steps-section').isInViewport()) {
+//         $('#menu').addClass('light-scheme').removeClass('mixed-scheme')
+//     } else if ($('#testimonial').isInViewport()) {
+//         $('#menu').addClass('fixed-scheme').removeClass('light-scheme')
+//     } else {
+//         $('#menu').removeClass('light-scheme').removeClass('fixed-scheme')
+//     }
+// });
+function openLogin() {
+    document.getElementById('auth__in').style.display = 'block';
+    document.getElementById('menu-links').style.display = 'none';
+    document.getElementById('demo-image').style.display = 'none';
+}
+// document.getElementById("btn-login").onclick = function () {
+//     document.getElementById('auth__in').style.display = 'block';
+//     document.getElementById('menu-links').style.display = 'none';
+//     document.getElementById('demo-image').style.display = 'none';
+// };
+// $('#btn-login').click(function () {
+//     $('.auth__in').show();
+//     $('#menu-links').hide();
+//     $('.demo-image').hide()
+// });
 
-$.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+// indicators.click(reloadVideos(),checkSlideShow());
+checkSlideShow();
 
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+// var indicatorsCollection = document.getElementsByClassName('carousel-indicators');
+// var indicators = indicatorsCollection[0].getElementsByTagName('li');
+function checkSlideShow() {
+    // if (indicators[0].classList.contains('active')) {
+    //     reloadVideos();
+    //     console.log('video reloaded')
+    // }
+    setTimeout(function() {
+        reloadVideos();
+        console.log('video reloaded');
+        checkSlideShow()
+    }, 5600*3);
+}
 
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-};
+function reloadVideos() {
+    var item = document.getElementById('setup-steps-carousel').getElementsByClassName('step-video');
+    for (i = 0; i < item.length; i++) {
+        item[i].load()
+    }
+}
+
+// $.fn.isInViewport = function() {
+//     var elementTop = $(this).offset().top;
+//     var elementBottom = elementTop + $(this).outerHeight();
+//
+//     var viewportTop = $(window).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
+//
+//     return elementBottom > viewportTop && elementTop < viewportBottom;
+// };
 
 function openMenu() {
     var x = document.getElementById("menu");
